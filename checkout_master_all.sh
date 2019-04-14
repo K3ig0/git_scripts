d347e6a3ed71c5d84dfad994ff14d7e065f9d594
K3ig0 <keigo.fic@gmail.com>
2019-04-14 22:02:41 +0200

    relocate scripts

--- checkout_master_all.sh
+++
@@ -1,6 +0,0 @@
-for file in $(find ./* -maxdepth 0 -type d); do
-    echo "$(basename "$file") :"
-	cd "$file"
-	git checkout master
-	cd ..
-done
--- echo_children_folders.sh
+++
@@ -1,3 +0,0 @@
-for file in $(find ./* -maxdepth 0 -type d); do
-    echo "$(basename "$file") :"
-done
